7201b614c4493dbd90ae7c149a623016071f50dd
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-10-03 18:10:00 +0100

    feat: move firefox user.js to dotfiles

---
+++ user/firefox/.mozilla/firefox/0.goldy-custom/user-overrides.js
@@ -0,0 +1,23 @@
+// Privacy overrides
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("browser.display.use_system_colors", true);
+
+// Preferences
+user_pref("browser.startup.page", 1);
+user_pref("browser.startup.homepage", "https://www.google.com");
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("privacy.clearOnShutdown.openWindows", false);
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
+
+// Weird fixes
+user_pref("widget.chrome.allow-gtk-dark-theme", true); // dark mode fix
+
+// OPSEC Preferences
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.openpage", false);
